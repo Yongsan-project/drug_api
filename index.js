0c7a39917bb5101412e7e2b7f9ab1715b7951c57
unknown <yjs12180825@gmail.com>
2023-09-15 23:28:00 +0900

    modify res header

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ app.use(cors({
     credentials: true
 }));
 app.all('/*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://www.yongsandrug.co.kr/");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Origin", "*"); res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, Authorization, Content-Length, X-Requested-With');
     next();
 });
 
