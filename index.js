10a638b6b4b57341bfe04da6db5668d0535db16f
unknown <yjs12180825@gmail.com>
2023-09-18 12:07:50 +0900

    header to setHeader - index.js

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ const logger = morgan("dev");
 // }));
 
 app.all('/*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://www.yongsandrug.co.kr/");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers',
+    res.setHeader("Access-Control-Allow-Origin", "https://www.yongsandrug.co.kr/");
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers',
         'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.header('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 });
 
