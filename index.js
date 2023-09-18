d2919e07b9ca6016c03970767c1d86275b1fe994
balhyo-younjisang <106881184+balhyo-younjisang@users.noreply.github.com>
2023-09-18 10:07:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.all('/*', function (req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers',
         'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Credentials', 'true'); 
     next();
 });
 
