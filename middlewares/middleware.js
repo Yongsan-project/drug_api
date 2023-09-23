1868fd73cb526f2dc3870f1d8056081afbbbd201
unknown <yjs12180825@gmail.com>
2023-09-23 19:45:54 +0900

    debug start

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -1,9 +1,11 @@
 export const protectorMiddleware = (req, res, next) => {
-    if (req.session) next();
-    else return res.status(402).json("Not allowed");
+    // if (req.session) next();
+    // else return res.status(402).json("Not allowed");
+    next();
 }
 
 export const UnknownUserMiddleware = (req, res, next) => {
-    if (req.session) return res.status(402).json("Not allowed");
+    // if (req.session) return res.status(402).json("Not allowed");
+    // next();
     next();
 }
